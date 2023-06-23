5f92c8d7afe677ea0ea7abb8dfbd94cd6acb9cd0
Edson Coelho <edcoelho.dev@gmail.com>
2023-06-23 20:31:24 -0300

    fixed pagination button colors

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 // Setting min-height of "main" tags
-let mainMinHeight = 'calc(100vh - ' + $('nav').css('height') + ' - ' + $('footer').css('margin-top') + ' - ' + $('footer').css('height') + ')';
+let mainMinHeight = 'calc(100vh - ' + $('#navbar').css('height') + ' - ' + $('footer').css('margin-top') + ' - ' + $('footer').css('height') + ')';
 $('main').css('min-height', mainMinHeight);
 
 // Setting "overflow: auto;" in "main" tags with "#tag-seacher" child.
