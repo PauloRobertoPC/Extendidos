041bb858f02edbbb50c0cc4dfa3510a61c0e4958
Edson Coelho <edcoelho.dev@gmail.com>
2023-06-23 19:36:12 -0300

    project tag list and job tag list pages

---
+++ static/main.js
@@ -0,0 +1,8 @@
+// Setting min-height of "main" tags
+let mainMinHeight = 'calc(100vh - ' + $('nav').css('height') + ' - ' + $('footer').css('margin-top') + ' - ' + $('footer').css('height') + ')';
+$('main').css('min-height', mainMinHeight);
+
+// Setting "overflow: auto;" in "main" tags with "#tag-seacher" child.
+if ($('main').find('#tag-searcher').length > 0) {
+    $('main').css('overflow', 'auto');
+}
